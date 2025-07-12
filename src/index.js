d6548ca21c86ef690dd9b0a716136b713f653260
Zero Projects <logensiva136@gmail.com>
2025-07-13 02:28:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProtectedRoutes from "./ProtectedRoutes";
 import { Login } from "./auth";
@@ -20,37 +19,6 @@ root.render(
         {/* Add other routes here as needed */}
       </Routes>
     </Router>
-=======
-
-import { RouterProvider, BrowserRouter, Routes, Route } from "react-router-dom";
-
-import ProtectedRoutes from "./ProtectedRoutes";
-import { Login } from "./auth.jsx";
-
-// const auth = getAuth()
-//   .projectConfigManager()
-//   .updateProjectConfig({
-//     multiFactorConfig: {
-//       providerConfigs: [
-//         {
-//           state: "ENABLED",
-//         },
-//       ],
-//     },
-//   });
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProtectedRoutes />}>
-          <Route path="/dashboard" element={<App />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 54da1e2444e4e2684bee973159cc90a22b56bdb6
   </React.StrictMode>
 );
 
